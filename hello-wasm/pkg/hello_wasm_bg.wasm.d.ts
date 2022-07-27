0ddff3c2d3fedb6c546aeb5ce27433d5c4ba5a12
Adam Quinton <quintonish@gmail.com>
2022-07-26 23:42:05 -0600

    add pkg

---
+++ hello-wasm/pkg/hello_wasm_bg.wasm.d.ts
@@ -0,0 +1,6 @@
+/* tslint:disable */
+/* eslint-disable */
+export const memory: WebAssembly.Memory;
+export function get_week_of_year(a: number, b: number): number;
+export function __wbindgen_malloc(a: number): number;
+export function __wbindgen_realloc(a: number, b: number, c: number): number;
